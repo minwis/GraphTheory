63e61c3d2d839cde27f37c5221ec7965527572b0
AhnMinsur <minwisdom08@gmail.com>
2023-03-22 11:36:26 +0530

    added node class

--- LinkedList.java
+++ LinkedList.java
@@ -1,22 +1,11 @@
+import java.util.*;
+
 public class LinkedList {
 
-
-    //every node have their value and pointer to the next node
-
-    /*first, initialize nodes which have values 1~10
-    node(value of 1) -> node(value of 2) -> node(value of 3) -> node(value of 4) -> node(value of 5)
-     -> node(value of 6) -> node(value of 7) -> node(value of 8) -> node(value of 9) -> node(value of 10)
-     */
-    /*goal: program which removes a node safely: search the desired node using recursion -> if the node is found,
-    replace "previous node's pointer" to "present node's pointer". set the present node to NULL.
-
-     */
-
-    public void node( /*parameter*/ ) {
-
-    }
+    public static List<node> list = new ArrayList<node>();
 
     public static void main(String args[]) {
-
+        //node start = new node
+        
     }
 }
---
+++ node.java
@@ -0,0 +1,10 @@
+public class node {
+
+    public int value = 0;
+    public node pointer = null;
+
+    public node(int value) {
+        this.value = value;
+
+    }
+}
