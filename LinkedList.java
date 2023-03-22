a19babef2cdf936903ccad7b8d209a1e1ff70afe
AhnMinsur <minwisdom08@gmail.com>
2023-03-22 22:41:46 +0530

    save progress of removingNode

--- LinkedList.java
+++ LinkedList.java
@@ -19,12 +19,14 @@ public class LinkedList {
         if ( Node.equals(head) && Node.value == removeValue ) {
             head = head.next;
         }
-        else if ( Node.next.value == removeValue ) {
+        else if ( Node.next != null && Node.next.value == removeValue ) {
             Node.next = Node.next.next;
         }
-        else {
-            removeNode(Node.next, removeValue);
+
+        if ( Node.next == null ) {
+            return;
         }
+        removeNode(Node.next, removeValue);
     }
 
     public static void removeNode(node Node, int index, int removeIndex) {
@@ -33,9 +35,7 @@ public class LinkedList {
         }
         else if ( index == removeIndex ) {
             Node.next = Node.next.next;
-        }
-
-        if ( Node.next != null ) {
+        }else {
             removeNode(Node.next, index + 1, removeIndex);
         }
 
@@ -50,7 +50,7 @@ public class LinkedList {
         }
     }
 
-    public static void addNode(node Node, node targetNode, int index, int targetIndex) { //the index starts with 0.
+    public static void addNode(node Node, node targetNode, int index, int targetIndex) {
         if ( index+1 == targetIndex ) {
             targetNode.next = Node.next;
             Node.next = targetNode;
@@ -62,33 +62,15 @@ public class LinkedList {
 
     public static void main(String args[]) {
 
-        /*node two = new node(2);
-        node three = new node(3);
-        node four = new node(4);
-        node five = new node(5);
-        node six = new node(6);
-        node seven = new node(7);
-        node eight = new node(8);
-        node nine = new node(9);
-        node ten = new node(10);
-
-        head.next = two;
-        two.next = three;
-        three.next = four;
-        four.next = five;
-        five.next = six;
-        six.next = seven;
-        seven.next = eight;
-        eight.next = nine;
-        nine.next = ten;*/
-
         for ( int i = 2; i <= 10; i++ ) {
             addNode(head, new node(i));
         }
 
-        //addNode(head, new node(4), 0, 4);
-        for ( int i = 1; i <= 10; i++ ) {
-            removeNode(head, i);
+        addNode(head, new node(10), 0, 10);
+        //removeNode(head, 10);
+
+        //for ( int i = 1; i <= 10; i++ ) {
+            removeNode(head, 10);
 
             node N = head;
             while (true) {
@@ -108,12 +90,11 @@ public class LinkedList {
             }
 
             System.out.println();
-        }
-        /*
-        Scanner sc = new Scanner(System.in);
+        //}
+
+        /*Scanner sc = new Scanner(System.in);
         int removeValue = sc.nextInt();
-        removeNode(head, 1);
-        removeNode(head, 2);
+        removeNode(head, removeValue);
 
         node N = head;
         while (true) {
